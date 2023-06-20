6c0c0a163a4664e871f79a98eb0d8d355dca6132
ankit12332 <ankit.panda010.ap@gmail.com>
2023-06-21 01:25:21 +0530

    ADDED A NAVBAR BUTTON FOR RESPONSIVE

--- src/components/dashboardComponent.js
+++ src/components/dashboardComponent.js
@@ -1,19 +1,28 @@
 'use client';
 import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+
 
 const Dashboard = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
 
   const handleItemClick = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleMobileMenuClick = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+  
+
   return (
     <div className="flex h-screen">
-      <div className="w-1/5 bg-gray-800 text-white">
-        {/* Sidebar */}
+      {/* Sidebar */}
+      <div className="w-1/5 bg-gray-800 text-white overflow-y-auto">
         <ul className="py-4">
-          <li className="px-4 py-2 hover:bg-gray-700" onClick={handleItemClick}>
+          <li className="px-4 py-2 pt-16 hover:bg-gray-700" onClick={handleItemClick}>
             Item 1
             {isOpen && (
               <ul className="ml-4 subitems">
@@ -27,27 +36,53 @@ const Dashboard = () => {
           <li className="px-4 py-2 hover:bg-gray-700">Item 3</li>
         </ul>
       </div>
-      <div className="w-4/5 bg-white">
+
+      {/* Main content */}
+      <div className="w-4/5 bg-white overflow-y-auto">
         {/* Navbar */}
-        <nav className="bg-gray-200 shadow">
-          <div className="flex items-center justify-between px-4 py-2">
-            <h1 className="text-lg font-bold text-gray-800">SOA UNIVERSITY</h1>
-            <ul className="flex space-x-4">
-              <li>
-                <a href="#" className="text-gray-800 hover:text-gray-600">Home</a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-800 hover:text-gray-600">About</a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-800 hover:text-gray-600">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <nav className="fixed top-0 left-0 right-0 bg-gray-200 shadow z-10">
+  <div className="flex items-center justify-between px-4 py-2">
+    <h1 className="text-lg font-bold text-gray-800">SOA UNIVERSITY</h1>
+    <div className="flex items-center">
+      <ul className="hidden md:flex space-x-4">
+        <li>
+          <a href="#" className="text-gray-800 hover:text-gray-600">Home</a>
+        </li>
+        <li>
+          <a href="#" className="text-gray-800 hover:text-gray-600">About</a>
+        </li>
+        <li>
+          <a href="#" className="text-gray-800 hover:text-gray-600">Contact</a>
+        </li>
+      </ul>
+      <div className="md:hidden">
+        <button
+          onClick={handleMobileMenuClick}
+          className="text-gray-800 hover:text-gray-600 focus:outline-none"
+        >
+          <FaBars />
+        </button>
+        {isMobileMenuOpen && (
+          <ul className="absolute right-0 mt-2 py-2 bg-white border rounded shadow-lg">
+            <li className="px-4 py-2">
+              <a href="#" className="text-gray-800 hover:text-gray-600">Home</a>
+            </li>
+            <li className="px-4 py-2">
+              <a href="#" className="text-gray-800 hover:text-gray-600">About</a>
+            </li>
+            <li className="px-4 py-2">
+              <a href="#" className="text-gray-800 hover:text-gray-600">Contact</a>
+            </li>
+          </ul>
+        )}
+      </div>
+    </div>
+  </div>
+</nav>
+
 
         {/* Main content */}
-        <div className="p-4">
+        <div className="p-4 pt-16">
           <h2 className="text-2xl font-bold text-gray-800 mb-4">Welcome to the Dashboard</h2>
           <p className="text-gray-600">This is the main content of your dashboard.</p>
         </div>
